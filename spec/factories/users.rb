4db4aa12e66b9d3e6676cf6f10895dad9c7e4e2b
Rodrigo Toledo <rodrigo.toledo@superare.co>
2020-03-11 01:20:34 -0300

    failure on tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 require 'faker'
 FactoryBot.define do
   factory :user do |user|
-    email { Faker::Internet.email }
-    password { '12345asdfg' }
-    password_confirmation { '12345asdfg' }
+    email { 'testeapple@ioasys.com.br' }
+    password { '12341234' }
+    password_confirmation { '12341234' }
   end
 end
