9928fedf196f7f876877072d4d034123df38fa19
Rodrigo Toledo <rodrigo.toledo@superare.co>
2020-03-10 23:36:35 -0300

    requests by companies with tests and results ok

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,18 @@
 class CompaniesController < ApplicationController
+  before_action :set_company
+
   def index
+    @companies = @company.filter_by_name_and_type(params[:name], params[:type])
+    render json: @companies
   end
 
   def show
+
   end
+
+  protected
+  def set_company
+    @company ||= Company.unscoped
+  end
+  
 end
--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -3,10 +3,44 @@ require 'rails_helper'
 RSpec.describe CompaniesController, type: :controller do
 
   describe "GET #index" do
+    before(:all) do
+      company = Company.create(name: "Filo")
+      company.medium_sized!
+
+      company = Company.create(name: "Filhotinhas")
+      company.startup!
+
+      company = Company.create(name: "Coca-colas")
+      company.large!
+    end
+    
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "assigns without parameters returning all registers" do
+      get :index
+      expect(assigns(:companies)).to have(3).items
+    end
+
+    it "assigns with parameter name with partial content and return 2 values" do
+      get :index, params: {name: 'Fi'}
+      expect(assigns(:companies)).to have(2).items
+    end
+
+    it "assigns with parameter name with partial content and type to return 1 value" do
+      get :index, params: {name: 'Fi', type: "startup"}
+      expect(assigns(:companies)).to have(1).items
+    end
+
+    it "test the result in json" do
+      get :index, params: {name: 'Filho'}
+      expected = assigns(:companies)
+      expect(response.body).to eql(expected.to_json)
+    end
+    
+    
   end
 
   describe "GET #show" do
